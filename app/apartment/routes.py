4c8d2d483061698688402edf885b6d65ef2a2fce
timothycao <tcao.pro@gmail.com>
2024-04-23 10:36:17 -0400

    Apartment: get_units, get_unit routes and templates

--- app/apartment/routes.py
+++ app/apartment/routes.py
@@ -55,3 +55,66 @@ def get_building(company_name, building_name):
         return render_template('apartment_building.html', building=building, units=units, pet_policies=pet_policies, amenities=amenities)
     else:
         return "Building not found"
+
+@apartment.route('/units', methods=['GET'])
+def get_units():
+    cursor = conn.cursor()
+
+    query = 'SELECT * FROM ApartmentUnit'
+    
+    if request.args:
+        where_clauses = []
+        params = []
+
+        for key, value in request.args.items():
+            where_clauses.append(f'{key}= %s')
+            params.append(value)
+        
+        if where_clauses:
+            query += ' WHERE ' + ' AND '.join(where_clauses)
+        
+        cursor.execute(query, params)
+    
+    else:
+        cursor.execute(query)
+
+    units = cursor.fetchall()
+    
+    cursor.close()
+    
+    return render_template('apartment_units.html', units=units)
+
+@apartment.route('/units/<unit_id>', methods=['GET'])
+def get_unit(unit_id):
+    cursor = conn.cursor()
+
+    get_unit_query = 'SELECT * FROM ApartmentUnit NATURAL JOIN ApartmentBuilding WHERE unit_id = %s'
+    cursor.execute(get_unit_query, (unit_id))
+    unit = cursor.fetchone()
+
+    get_rooms_query = 'SELECT * FROM Room WHERE unit_id = %s'
+    cursor.execute(get_rooms_query, (unit_id))
+    rooms = cursor.fetchall()
+
+    get_unit_amenities_query = 'SELECT * FROM Amenity NATURAL JOIN UnitAmenity WHERE unit_id = %s'
+    cursor.execute(get_unit_amenities_query, (unit_id))
+    unit_amenities = cursor.fetchall()
+
+    # Building details
+    company_name = unit['company_name']
+    building_name = unit['building_name']
+
+    get_pet_policies_query = 'SELECT * FROM PetPolicy WHERE company_name = %s AND building_name = %s'
+    cursor.execute(get_pet_policies_query, (company_name, building_name))
+    pet_policies = cursor.fetchall()
+
+    get_building_amenities_query = 'SELECT * FROM Amenity NATURAL JOIN BuildingAmenity WHERE company_name = %s AND building_name = %s'
+    cursor.execute(get_building_amenities_query, (company_name, building_name))
+    building_amenities = cursor.fetchall()
+
+    cursor.close()
+
+    if unit:
+        return render_template('apartment_unit.html', unit=unit, rooms=rooms, unit_amenities=unit_amenities, pet_policies=pet_policies, building_amenities=building_amenities)
+    else:
+        return "Unit not found"
