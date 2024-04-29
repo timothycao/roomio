1d7e733eb87e34efed6447f2fce033f28185a049
timothycao <tcao.pro@gmail.com>
2024-04-29 15:05:15 -0400

    Apartment: added amenities to get_units route (advanced search)

--- app/apartment/routes.py
+++ app/apartment/routes.py
@@ -88,20 +88,44 @@ def get_building(company_name, building_name):
 def get_units():
     cursor = conn.cursor()
 
+    get_building_amenities_query = 'SELECT DISTINCT amenity_name FROM BuildingAmenity'
+    cursor.execute(get_building_amenities_query)
+    building_amenities = cursor.fetchall()
+
+    get_unit_amenities_query = 'SELECT DISTINCT amenity_name FROM UnitAmenity'
+    cursor.execute(get_unit_amenities_query)
+    unit_amenities = cursor.fetchall()
+
+    # bedroom and bathroom count
+    with_clause = (
+        'WITH RoomCount AS ('
+            'SELECT au.unit_id, '
+            'SUM(CASE WHEN r.room_name LIKE %s THEN 1 ELSE 0 END) AS bedroom_count, '
+            'SUM(CASE WHEN r.room_name LIKE %s THEN 1 ELSE 0 END) AS bathroom_count '
+            'FROM ApartmentUnit au JOIN Room r ON au.unit_id = r.unit_id '
+            'GROUP BY au.unit_id'
+        ')'
+    )
     select_clause = (
-        'SELECT au.company_name AS company_name, '
+        ' SELECT au.company_name AS company_name, '
         'au.building_name AS building_name, '
         'au.unit_id AS unit_id, '
         'au.unit_number AS unit_number, '
         'au.monthly_rent AS monthly_rent, '
         'au.unit_size AS unit_size, '
         'au.available_date AS available_date, '
-        'SUM(CASE WHEN r.room_name LIKE %s THEN 1 ELSE 0 END) AS bedroom_count, '
-        'SUM(CASE WHEN r.room_name LIKE %s THEN 1 ELSE 0 END) AS bathroom_count'
+        'rc.bedroom_count AS bedroom_count, '
+        'rc.bathroom_count AS bathroom_count, '
+        'GROUP_CONCAT(DISTINCT ua.amenity_name SEPARATOR %s) AS unit_amenities, '
+        'GROUP_CONCAT(DISTINCT ba.amenity_name SEPARATOR %s) AS building_amenities'
     )
-    from_clause = ' FROM ApartmentUnit AS au NATURAL JOIN Room AS r'
+    from_clause = (
+        ' FROM ApartmentUnit AS au NATURAL JOIN RoomCount AS rc '
+        'LEFT JOIN UnitAmenity AS ua ON au.unit_id = ua.unit_id '
+        'LEFT JOIN BuildingAmenity AS ba ON au.company_name = ba.company_name AND au.building_name = ba.building_name'
+    )
     conditions = []
-    params = [f'bedroom%', f'bathroom%']
+    params = [f'bedroom%', f'bathroom%', ', ', ', ']
     group_by_clause = ' GROUP BY au.unit_id'
 
     pet_count = 0
@@ -160,6 +184,35 @@ def get_units():
                 conditions.append('ab.zip_code = %s')
                 params.append(zip_code)
 
+        # amenities
+
+        selected_building_amenities = request.args.getlist('building_amenities')
+        if selected_building_amenities:
+            condition = '(SELECT COUNT(DISTINCT amenity_name) FROM BuildingAmenity WHERE company_name = au.company_name AND building_name = au.building_name AND amenity_name IN ('
+            for i, building_amenity in enumerate(selected_building_amenities):
+                if i == len(selected_building_amenities) - 1:
+                    condition += '%s'
+                else:
+                    condition += '%s, '
+                params.append(building_amenity)
+            condition += ')) = %s'
+            conditions.append(condition)
+            params.append(len(selected_building_amenities))
+
+        selected_unit_amenities = request.args.getlist('unit_amenities')
+        if selected_unit_amenities:
+            condition = '(SELECT COUNT(DISTINCT amenity_name) FROM UnitAmenity WHERE unit_id = au.unit_id AND amenity_name IN ('
+            for i, unit_amenity in enumerate(selected_unit_amenities):
+                if i == len(selected_unit_amenities) - 1:
+                    condition += '%s'
+                else:
+                    condition += '%s, '
+                params.append(unit_amenity)
+            condition += ')) = %s'
+            conditions.append(condition)
+            params.append(len(selected_unit_amenities))
+
+        # pet policies
         if 'username' in session and 'pets' in session:
             pet_count = len(session['pets'])
             pet_conditions = []
@@ -173,7 +226,7 @@ def get_units():
             if pet_conditions:
                 conditions.append(f'({' AND '.join(pet_conditions)})')
 
-    query = select_clause + from_clause
+    query = with_clause + select_clause + from_clause
     if conditions:
         query += f' WHERE {' AND '.join(conditions)}'
     query += group_by_clause
@@ -186,7 +239,7 @@ def get_units():
     # print('query: ', query, flush=True)
     # print('units: ', units, flush=True)
 
-    return render_template('apartment_units.html', units=units, pet_count=pet_count, has_searched=bool(request.args))
+    return render_template('apartment_units.html', units=units, pet_count=pet_count, building_amenities=building_amenities, unit_amenities=unit_amenities, has_searched=bool(request.args))
 
 @apartment.route('/units/<unit_id>', methods=['GET'])
 def get_unit(unit_id):
